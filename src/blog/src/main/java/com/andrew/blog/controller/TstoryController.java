ecf92cef7ca634d70bba4a73a94017ead7a4de7c
임석현 <sjsh1623@flow.team>
2022-03-19 14:41:11 +0900

    불필요한 libaray 제거

--- src/blog/src/main/java/com/andrew/blog/controller/TstoryController.java
+++ src/blog/src/main/java/com/andrew/blog/controller/TstoryController.java
@@ -1,17 +1,10 @@
 package com.andrew.blog.controller;
 
-import com.andrew.blog.dto.Response;
 import com.andrew.blog.service.TstoryService;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("Tstory")
